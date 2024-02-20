3a9e0e48c98856da701124170cd1dfdcbdca0539
magiodev <31893902+magiodev@users.noreply.github.com>
2024-02-20 19:58:21 +0100

    Fix nomenclature

--- packages/osmosis-test-tube/src/runner/app.rs
+++ packages/osmosis-test-tube/src/runner/app.rs
@@ -81,8 +81,8 @@ impl OsmosisTestApp {
 
     /// Initialize account with initial balance of any coins.
     /// This function mints new coins and send to newly created account
-    pub fn init_account_from_seed(&self, coins: &[Coin], mnemonic: &str) -> RunnerResult<SigningAccount> {
-        self.inner.init_account_from_seed(coins, mnemonic)
+    pub fn init_account_from_mnemonic(&self, coins: &[Coin], mnemonic: &str) -> RunnerResult<SigningAccount> {
+        self.inner.init_account_from_mnemonic(coins, mnemonic)
     }
 
     /// Convinience function to create multiple accounts with the same
